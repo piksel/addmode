0487c0e34d09d669be21767d84e2e161c6dba06a
Nils <nils@piksel.se>
2014-05-28 08:48:13 +0200

    initial github upload

---
+++ FormADDMode.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace ADDMode
+{
+    partial class FormADDMode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormADDMode));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiMoveToScreen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAlpha = new System.Windows.Forms.ToolStripMenuItem();
+            this.clickThroughToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.customToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiMoveToScreen,
+            this.tsmiAlpha,
+            this.clickThroughToolStripMenuItem,
+            this.alwaysOnTopToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 120);
+            // 
+            // tsmiMoveToScreen
+            // 
+            this.tsmiMoveToScreen.Name = "tsmiMoveToScreen";
+            this.tsmiMoveToScreen.Size = new System.Drawing.Size(152, 22);
+            this.tsmiMoveToScreen.Text = "Move to screen";
+            // 
+            // tsmiAlpha
+            // 
+            this.tsmiAlpha.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem2,
+            this.customToolStripMenuItem});
+            this.tsmiAlpha.Name = "tsmiAlpha";
+            this.tsmiAlpha.Size = new System.Drawing.Size(152, 22);
+            this.tsmiAlpha.Text = "Alpha";
+            // 
+            // clickThroughToolStripMenuItem
+            // 
+            this.clickThroughToolStripMenuItem.Name = "clickThroughToolStripMenuItem";
+            this.clickThroughToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clickThroughToolStripMenuItem.Text = "Click through";
+            this.clickThroughToolStripMenuItem.Click += new System.EventHandler(this.clickThroughToolStripMenuItem_Click);
+            // 
+            // alwaysOnTopToolStripMenuItem
+            // 
+            this.alwaysOnTopToolStripMenuItem.Checked = true;
+            this.alwaysOnTopToolStripMenuItem.CheckOnClick = true;
+            this.alwaysOnTopToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+            this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.alwaysOnTopToolStripMenuItem.Text = "Always on top";
+            this.alwaysOnTopToolStripMenuItem.Click += new System.EventHandler(this.alwaysOnTopToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.CheckOnClick = true;
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem3.Tag = "1.0";
+            this.toolStripMenuItem3.Text = "100%";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.CheckOnClick = true;
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem4.Tag = "0.75";
+            this.toolStripMenuItem4.Text = "75%";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.CheckOnClick = true;
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem5.Tag = "0.50";
+            this.toolStripMenuItem5.Text = "50%";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.CheckOnClick = true;
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem6.Tag = "0.25";
+            this.toolStripMenuItem6.Text = "25%";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.CheckOnClick = true;
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem7.Tag = "0.10";
+            this.toolStripMenuItem7.Text = "10%";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // customToolStripMenuItem
+            // 
+            this.customToolStripMenuItem.Enabled = false;
+            this.customToolStripMenuItem.Name = "customToolStripMenuItem";
+            this.customToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.customToolStripMenuItem.Text = "Custom...";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ADD Mode";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ADD Mode";
+            this.Deactivate += new System.EventHandler(this.FormADDMode_Deactivate);
+            this.Load += new System.EventHandler(this.FormADDMode_Load);
+            this.Leave += new System.EventHandler(this.FormADDMode_Leave);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiMoveToScreen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAlpha;
+        private System.Windows.Forms.ToolStripMenuItem clickThroughToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem customToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ FormADDMode.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Management;
+using System.Globalization;
+
+namespace ADDMode
+{
+    public partial class FormADDMode : Form
+    {
+
+#region Imports
+        [DllImport("user32.dll")]
+        static extern bool EnumDisplayDevices(string lpDevice, uint iDevNum, ref DISPLAY_DEVICE lpDisplayDevice, uint dwFlags);
+
+        [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Ansi)]
+        public struct DISPLAY_DEVICE 
+        {
+              [MarshalAs(UnmanagedType.U4)]
+              public int cb;
+              [MarshalAs(UnmanagedType.ByValTStr, SizeConst=32)]
+              public string DeviceName;
+              [MarshalAs(UnmanagedType.ByValTStr, SizeConst=128)]
+              public string DeviceString;
+              [MarshalAs(UnmanagedType.U4)]
+              public DisplayDeviceStateFlags StateFlags;
+              [MarshalAs(UnmanagedType.ByValTStr, SizeConst=128)]
+              public string DeviceID;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst=128)]
+              public string DeviceKey;
+        }
+
+        [Flags()]
+        public enum DisplayDeviceStateFlags : int
+        {
+            /// <summary>The device is part of the desktop.</summary>
+            AttachedToDesktop = 0x1,
+            MultiDriver = 0x2,
+            /// <summary>The device is part of the desktop.</summary>
+            PrimaryDevice = 0x4,
+            /// <summary>Represents a pseudo device used to mirror application drawing for remoting or other purposes.</summary>
+            MirroringDriver = 0x8,
+            /// <summary>The device is VGA compatible.</summary>
+            VGACompatible = 0x10,
+            /// <summary>The device is removable; it cannot be the primary display.</summary>
+            Removable = 0x20,
+            /// <summary>The device has more display modes than its output devices support.</summary>
+            ModesPruned = 0x8000000,
+            Remote = 0x4000000,
+            Disconnect = 0x2000000
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        /// <summary>
+        /// Changes an attribute of the specified window. The function also sets the 32-bit (long) value at the specified offset into the extra window memory.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window and, indirectly, the class to which the window belongs..</param>
+        /// <param name="nIndex">The zero-based offset to the value to be set. Valid values are in the range zero through the number of bytes of extra window memory, minus the size of an integer. To set any other value, specify one of the following values: GWL_EXSTYLE, GWL_HINSTANCE, GWL_ID, GWL_STYLE, GWL_USERDATA, GWL_WNDPROC </param>
+        /// <param name="dwNewLong">The replacement value.</param>
+        /// <returns>If the function succeeds, the return value is the previous value of the specified 32-bit integer.
+        /// If the function fails, the return value is zero. To get extended error information, call GetLastError. </returns>
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        // P/Invoke constants
+        private const int WM_SYSCOMMAND = 0x112;
+        private const int MF_STRING = 0x0;
+        private const int MF_SEPARATOR = 0x800;
+
+        // P/Invoke declarations
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool AppendMenu(IntPtr hMenu, int uFlags, int uIDNewItem, string lpNewItem);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool InsertMenu(IntPtr hMenu, int uPosition, int uFlags, int uIDNewItem, string lpNewItem);
+
+
+        // ID for the About item on the system menu
+        private int SYSMENU_CLICKTHROUGH_ID = 0x1;
+        private int initialStyle = -1;
+        private CultureInfo ciEnUs;
+
+
+#endregion
+
+        public FormADDMode()
+        {
+            InitializeComponent();
+        }
+
+        private void FormADDMode_Load(object sender, EventArgs e)
+        {
+            for(int i=0; i < Screen.AllScreens.Length; i++)
+            {
+                var s = Screen.AllScreens[i];
+                var dspName = GetDisplayName(s.DeviceName);
+                Debug.WriteLine(dspName);
+                var tsmi = new ToolStripMenuItem(string.Format("#{0} {1} ({2}x{3})", (i + 1), dspName, s.Bounds.Width, s.Bounds.Height));
+                tsmi.Tag = i;
+                tsmi.Click += new EventHandler(tsmiScreenX_clicked);
+                tsmiMoveToScreen.DropDownItems.Add(tsmi);
+            }
+            MoveToScreen(Screen.PrimaryScreen);
+            TopMost = true;
+            ciEnUs = CultureInfo.GetCultureInfo("en-us");
+        }
+
+        void tsmiScreenX_clicked(object sender, EventArgs e)
+        {
+            int sid = (int) ((ToolStripMenuItem)sender).Tag;
+            if(Screen.AllScreens.Length >= sid)
+                MoveToScreen(Screen.AllScreens[sid]);
+        }
+
+        private object GetDisplayName(string p)
+        {
+            var device = new DISPLAY_DEVICE();
+            device.cb = Marshal.SizeOf(device);
+            try
+            {
+                for (uint id = 0; EnumDisplayDevices(null, id, ref device, 0); id++)
+                { 
+                    device.cb = Marshal.SizeOf(device);
+
+                    if (device.DeviceName == p)
+                    {
+                        string card = device.DeviceString;
+
+                        EnumDisplayDevices(device.DeviceName, 0, ref device, 0);
+
+                        string monitor = device.DeviceString;
+
+                        return String.Format("{0} on {1}", monitor, card);
+                    }
+
+                }
+                return "Unknown";
+            }
+            catch (Exception ex)
+            {
+                Debug.Print("{0}", ex.ToString());
+                return "Unknown";
+            }
+        }
+
+        private void MoveToScreen(Screen screen)
+        {
+            Bounds = screen.Bounds;
+
+            int sid = -1;
+            for(int i=0; i < Screen.AllScreens.Length; i++)
+            {
+                if (Screen.AllScreens[i] == screen)
+                {
+                    sid = i;
+                }
+            }
+            foreach (ToolStripMenuItem tsmi in tsmiMoveToScreen.DropDownItems)
+            {
+                tsmi.Checked = ((int)tsmi.Tag == sid);
+            }
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void alwaysOnTopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TopMost = alwaysOnTopToolStripMenuItem.Checked;
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            var tsmiClicked = (ToolStripMenuItem)sender;
+            double alpha = double.Parse(tsmiClicked.Tag.ToString(), ciEnUs);
+            Opacity = alpha;
+
+            foreach (ToolStripItem tsi in tsmiAlpha.DropDownItems)
+            {
+                if(tsi.GetType() != typeof(ToolStripMenuItem)) continue;
+                ToolStripMenuItem tsmi = (ToolStripMenuItem)tsi;
+                tsmi.Checked = (tsmi == tsmiClicked);
+            }
+        }
+
+        private void clickThroughToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (initialStyle == -1)
+            {
+                initialStyle = GetWindowLong(this.Handle, -20);
+                SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+            }
+            else
+            {
+                SetWindowLong(this.Handle, -20, initialStyle);
+                initialStyle = -1;
+            }
+            clickThroughToolStripMenuItem.Checked = (initialStyle != -1);
+        }
+
+        private void FormADDMode_Leave(object sender, EventArgs e)
+        {
+            //BringToFront();
+        }
+
+        private void FormADDMode_Deactivate(object sender, EventArgs e)
+        {
+            //BringToFront();
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            // get handle to window's system menu
+            IntPtr hSysMenu = GetSystemMenu(this.Handle, false);
+
+            // separator
+            AppendMenu(hSysMenu, MF_SEPARATOR, 0, "");
+
+            // clickthrough menu item
+            AppendMenu(hSysMenu, MF_STRING, SYSMENU_CLICKTHROUGH_ID, "&Disable clickthrough");
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if ((m.Msg == WM_SYSCOMMAND) && ((int)m.WParam == SYSMENU_CLICKTHROUGH_ID))
+            {
+                if (initialStyle != -1)
+                {
+                    SetWindowLong(this.Handle, -20, initialStyle);
+                    initialStyle = -1;
+                }
+            }
+
+        }
+    }
+}
